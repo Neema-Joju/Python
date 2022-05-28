82d8cc83a9bc7bdb539b5d8cf74933e8a8f6d88d
Neema Joju <neemajoju96@gmail.com>
2022-05-28 13:05:59 +0530

    Add files via upload

---
+++ Exc3.py
@@ -0,0 +1,4 @@
+a=float(input("Enter the width of the room "))
+b=float(input("Enter the length of the room "))
+area= a*b
+print(area)
---
+++ Exc4.py
@@ -0,0 +1,5 @@
+w=float(input("Enter the width of the field:"))
+l=float(input("Enter the length of the field:"))
+area=w*l
+area/=43560
+print(area)
